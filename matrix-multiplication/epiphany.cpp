91816fdb016a8a0a1ab1defce148eac7e96d2bce
Ronny <ronnybrendel@gmail.com>
2014-10-05 11:52:16 +0000

    beautification
    * retab
    * more spaces
    * more consistent spacing
    * fewer capital letters
    * generally, less madness

--- matrix-multiplication/epiphany.cpp
+++ matrix-multiplication/epiphany.cpp
@@ -6,101 +6,96 @@
 #define B(i,j) b_mat[j*N+i]
 #define C(i,j) c_mat[j*N+i]
 
-float  a_mat[N*N] __attribute__ ((section (".data_bank1")));//result matrix
-float  b_mat[N*N] __attribute__ ((section (".data_bank2")));//result matrix
-float  c_mat[N*N] __attribute__ ((section (".data_bank3")));//result matrix
+float a_mat[N*N] __attribute__ ((section (".data_bank1"))); // result matrix
+float b_mat[N*N] __attribute__ ((section (".data_bank2"))); // result matrix
+float c_mat[N*N] __attribute__ ((section (".data_bank3"))); // result matrix
 
-float sfrand(void);
+static float sfrand();
 
 static unsigned int mirand =1;
 
 unsigned e_test_init(){
+	int *pass = (int*) 0x24; // use user interrupt entry for now
+	*pass = 0x00000000;      // initialize as "started"
 
-  int *pass = (int*)0x24;      //use user interrupt entry for now
-  *pass   = 0x00000000;        //initialize as "started"
+	// resetting mask register
+	__asm__ __volatile__ ("MOVTS IMASK, %0" : : "r" (0x0));
 
-  //Resetting mask register
-  __asm__ __volatile__ ("MOVTS IMASK, %0" : : "r" (0x0));
-
-  //Get the coreID using assembly
-  register unsigned coreid_in_reg asm("r0");
-  __asm__ __volatile__ ("MOVFS %0, COREID" : : "r" (coreid_in_reg));
-  coreid_in_reg = coreid_in_reg << 20;
-  return (coreid_in_reg);
+	// get the coreID using assembly
+	register unsigned coreid_in_reg asm("r0");
+	__asm__ __volatile__ ("MOVFS %0, COREID" : : "r" (coreid_in_reg));
+	coreid_in_reg = coreid_in_reg << 20;
+	return coreid_in_reg;
 }
 
-int e_test_finish(int status){
-
-  int *pass = (int*)0x24;    //overwrite the sync ivt entry on exit
-  if(status==1){
-    *pass = 0x12345678;
-  }
-  else{
-    *pass = 0xDEADBEEF;
-  }
-  while(1);
+int e_test_finish(int status) {
+	int *pass = (int*) 0x24; // overwrite the sync ivt entry on exit
+	if (status == 1) {
+		*pass = 0x12345678;
+	} else {
+		*pass = 0xDEADBEEF;
+	}
+	while(1);
 }
 
-void e_write_ack(unsigned *addr){
-  unsigned probe_data;
-  probe_data = (*(addr));              //read old data
-  probe_data = probe_data ^ 0xFFFFFFFF;//toggle old data
-  (*(addr)) = probe_data;              //write new toggled data
-  while(probe_data != *(addr)){        //keep reading until match is met
-  }
+void e_write_ack(unsigned* addr) {
+	unsigned probe_data;
+	probe_data = *(addr);                // read old data
+	probe_data = probe_data ^ 0xFFFFFFFF;// toggle old data
+	(*(addr)) = probe_data;              // write new toggled data
+	while(probe_data != *(addr)){        // keep reading until match is met
+	}
 }
 
-int main(int argc, char *argv[]){
-  int      i,j,k;
-  float    sum  = 0.f;
-  int      status = 1;
-  unsigned coreID;
+int main(int argc, char** args) {
+	int      i, j, k;
+	float    sum = 0.0f;
+	int      status = 1;
+	unsigned coreID;
 
-  //Test Init
-  coreID=e_test_init();
+	// test init
+	coreID = e_test_init();
 
-  //Fill input matrices with a constant
-  for (i=0; i<N; i++){
-     for (j=0; j<N; j++){
-       A(i,j) = sfrand();
-       B(i,j) = sfrand();
-     }
-  }
+	// fill input matrices with a constant
+	for (i = 0; i < N; i += 1) {
+		for (j = 0; j < N; j += 1) {
+			A(i,j) = sfrand();
+			B(i,j) = sfrand();
+		}
+	}
 
-  //Run matrix multiplication
-  for (i=0; i<N; i++){
-    for (j=0; j<N; j++){
-      C(i,j) = 0;
-      for (k=0; k<N; k++){
-         C(i,j) += A(i,k)*B(k,j);
-      }
-    }
-  }
+	// run matrix multiplication
+	for (i = 0; i < N; i+= 1) {
+		for (j = 0; j < N; j += 1) {
+			C(i,j) = 0;
+			for (k = 0; k < N; k += 1) {
+				C(i,j) += A(i,k) * B(k,j);
+			}
+		}
+	}
 
-  //Sum up the C matrix
-  for (i=0; i<N; i++){
-    for (j=0; j<N; j++){
-      sum+=C(i,j);
-    }
-  }
+	// sum up the C matrix
+	for (i = 0; i < N; i++) {
+		for (j = 0; j < N; j++) {
+			sum += C(i,j);
+		}
+	}
 
-  //Compare to expected result
-  // printf("sum=%f\n",sum);
+	// compare to expected result
+	// printf("sum=%f\n",sum);
 
-  //sum!=-9.114673f
-  if(sum!=-9.114673f){//testing for bit exact sum from Epiphany golden chip(s)
-    status=0;         //fail
-  }
-  //Finish Test
-  return e_test_finish(status);
+	if(sum != -9.114673f) { // testing for bit exact sum from Epiphany golden chip(s)
+		status=0;         // fail
+	}
+
+	// finish test
+	return e_test_finish(status);
 }
 
-//Weird and cool random floating point generator from:
-//www.rgba.org/articles/sfrand/sfrand.htm
-float sfrand( void){
-  unsigned int a;
-  mirand *=16807;
-  a = (mirand & 0x007fffff) | 0x40000000;
-  return ( *((float*)&a) - 3.0f );
+// weird and cool random floating point generator from www.rgba.org/articles/sfrand/sfrand.htm
+float sfrand() {
+	unsigned int a;
+	mirand *= 16807;
+	a = (mirand & 0x007fffff) | 0x40000000;
+	return *((float*)&a) - 3.0f;
 }
-
--- matrix-multiplication/host.cpp
+++ matrix-multiplication/host.cpp
@@ -7,123 +7,110 @@
 #include <e-hal.h>
 #include <e-loader.h>
 
-void e_check_test(void *dev, unsigned row, unsigned col, int *status);
-void usage();
+static void e_check_test(void* dev, unsigned row, unsigned col, int* status);
+static void usage();
 
-int main(int argc, char *argv[]){
+int main(int argc, char** args) {
+	e_loader_diag_t e_verbose;
+	e_platform_t platform;
+	e_epiphany_t dev;
+	int row0, col0, rows, cols, para;
+	char elfFile[4096];
+	int status = 1; // pass
+	int i, j;
 
-  e_loader_diag_t e_verbose;
-  e_platform_t platform;
-  e_epiphany_t dev;
-  int row0,col0,rows,cols,para;
-  char elfFile[4096];
-  int status=1;//pass
-  int i,j;
+	if (argc < 5) {
+		usage();
+		status = 0;
+	} else {
+		row0 = atoi(args[1]);
+		col0 = atoi(args[2]);
+		rows = atoi(args[3]);
+		cols = atoi(args[4]);
+		para = atoi(args[5]);
+		strcpy(elfFile, args[6]);
 
-  if (argc < 5){
-    usage();
-    status=0;
-  }
-  else{
-    row0    = atoi(argv[1]);
-    col0    = atoi(argv[2]);
-    rows    = atoi(argv[3]);
-    cols    = atoi(argv[4]);
-    para    = atoi(argv[5]);
-    strcpy(elfFile, argv[6]);
+		// initalize epiphany device
+		e_init(nullptr);
+		e_reset_system();
+		e_get_platform_info(&platform);
+		// e_set_loader_verbosity(L_D3);
+		e_open(&dev, 0, 0, platform.rows, platform.cols); //open all cores
 
-    //Initalize Epiphany device
-    e_init(NULL);
-    e_reset_system();
-    e_get_platform_info(&platform);
-    //e_set_loader_verbosity(L_D3);
-    e_open(&dev, 0, 0, platform.rows, platform.cols); //open all cores
+		// load program one at a time, checking one a time
+		if(para) {
+			printf("running in parallel\n");
+			for (i = row0; i < row0 + rows; i += 1) {
+				for (j = col0; j < col0 + cols; j += 1) {
+					e_load_group(elfFile, &dev, i, j, 1, 1, E_TRUE);
+				}
+			}
+		} else {
+			e_load_group(elfFile, &dev, row0, col0, (row0+rows), (col0+cols), E_TRUE);
+		}
 
-    //Load program one at a time, checking one a time
-    if(para){
-      printf("Running in parallel\n");
-      for (i=row0; i<(row0+rows); i++) {
-	for (j=col0; j<(col0+cols); j++) {
-	  e_load_group(elfFile, &dev, i, j, 1, 1, E_TRUE);
+		// checking the test
+		for (i = row0; i < row0 + rows; i += 1) {
+			for (j = col0; j < col0 + cols; j += 1) {
+				e_check_test(&dev, i, j, &status);
+			}
+		}
+
+		// close down Epiphany device
+		e_close(&dev);
+		e_finalize();
 	}
-      }
-    }
-    else{
-      e_load_group(elfFile, &dev, row0, col0, (row0+rows), (col0+cols), E_TRUE);
-    }
-    //Checking the test
-    for (i=row0; i<(row0+rows); i++) {
-      for (j=col0; j<(col0+cols); j++) {
-	e_check_test(&dev, i, j, &status);
-      }
-    }
 
-
-    //Close down Epiphany device
-    e_close(&dev);
-    e_finalize();
-  }
-
-  //self check
-  if(status){
-    return EXIT_SUCCESS;
-  }
-  else{
-    return EXIT_FAILURE;
-  }
+	// self check
+	if (status) {
+		return EXIT_SUCCESS;
+	} else{
+		return EXIT_FAILURE;
+	}
 }
-/**********************************************************************/
-void e_check_test( void *dev,
-		   unsigned row,
-		   unsigned col,
-		   int *status
-		  ){
 
-  unsigned int result;
-  int wait=1;
+void e_check_test(void* dev, unsigned row, unsigned col, int* status) {
+	unsigned int result;
+	int wait = 1;
 
-  while(1){
-    e_read(dev,row, col, 0x24, &result, sizeof(unsigned));
-    if(result==0xDEADBEEF){
-      printf("Core (%d,%d) FAILED\n",row,col);
-      *status=0;
-      break;
-    }
-    else if(result==0x12345678){
-      unsigned clr= ( unsigned ) 0x0;
-      e_write(dev,row, col, 0x24, &clr, sizeof(clr));
-      printf("Core (%d,%d) PASSED\n",row,col);
-      break;
-    }
-    else{
-      if(wait){
-	usleep(10000);
-	printf("Core (%d,%d) WAITING...\n",row,col);
-	wait=0;
-      }
-      else{
-	printf("Core (%d,%d) FAILED\n",row,col);
-	*status=0;
-	break;
-      }
-    }
-  }
+	while(1) {
+		e_read(dev, row, col, 0x24, &result, sizeof(unsigned));
+		if (result == 0xdeadbeef) {
+			printf("core (%d,%d) failed\n",row,col);
+			*status = 0;
+			break;
+		} else if (result==0x12345678) {
+			unsigned clr = (unsigned) 0x0;
+			e_write(dev, row, col, 0x24, &clr, sizeof(clr));
+			printf("core %d,%d passed\n", row, col);
+			break;
+		} else{
+			if (wait){
+				usleep(10000);
+				printf("core %d,%d waiting...\n", row, col);
+				wait = 0;
+			} else {
+				printf("core %d,%d failed\n", row, col);
+				*status = 0;
+				break;
+			}
+		}
+	}
 }
-/**********************************************************************/
-void usage(){
- printf("-----------------------------------------------\n");
-  printf("Function: Runs exhaustive march-c memory test\n");
-  printf("Usage:    e-test <row> <col> <rows> <cols> <elf>\n");
-  printf("Example:  e-test 0 0 4 4 1 my.elf\n");
-  printf("\n");
-  printf("Options:\n");
-  printf("  row     - target core start row coordinate\n");
-  printf("  col     - target core start column coordinate\n");
-  printf("  rows    - number of rows to test\n");
-  printf("  cols    - number of columns to test\n");
-  printf("  para    - run test in parallel\n");
-  printf("  elf     - path to elf file\n");
-  printf("-----------------------------------------------\n");
-  return;
 
+void usage() {
+	printf(
+			"matrix-multiplication-host <row> <col> <rows> <cols> <srec>\n"
+			"\n"
+			"\texample:\n"
+			"\t\te-test 0 0 4 4 1 my.srec\n"
+			"\n"
+			"\toptions:\n"
+			"\t\trow     target core start row coordinate\n"
+			"\t\tcol     target core start column coordinate\n"
+			"\t\trows    number of rows to test\n"
+			"\t\tcols    number of columns to test\n"
+			"\t\tpara    run test in parallel\n"
+			"\t\tsrec    path to srec file\n"
+			);
 }
