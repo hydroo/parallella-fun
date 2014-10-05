b91fcfb524388f6c9dd3464f599338712cf83b68
Ronny <ronnybrendel@gmail.com>
2014-10-05 11:26:19 +0000

    enabled c++14 on the host

--- matrix-multiplication/host.cpp
+++ matrix-multiplication/host.cpp
@@ -1,6 +1,9 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
+#include <cstdlib>
+#include <cstdio>
+#include <cstring>
+
+#include <unistd.h>
+
 #include <e-hal.h>
 #include <e-loader.h>
 
